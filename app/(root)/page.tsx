c1e05f8d77f0fbe1c55d37fc253c33fc13abedc7
wediomer <wediomer2020@gmail.com>
2023-12-18 11:56:12 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+
+ 
+export default function Home() {
+  return (
+    <>
+      <h1 className="head-text text-left">Seid</h1>
+    </>
+  )
+}
---
+++ components/shared/LeftSiderbar.tsx
@@ -0,0 +1,5 @@
+function LeftSidebar(){
+    return <h1>LeftSiderbar</h1>
+}
+
+export default LeftSidebar;
