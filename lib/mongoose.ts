3968acdb5d0343ebd8d68c640d09691b6ea9087e
Mhreteab Adresom <mhreteabadresom@gmail.com>
2024-01-05 18:59:11 +0300

    Update in the Home page and in the create thread

---
+++ lib/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+let isconnected = false;
+
+export const connectToDB = async ()=>{
+    mongoose.set('strictQuery', true);
+    if(!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+    if(isconnected)return console.log('Already connected to Mongodb');
+    
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        isconnected= true;
+        console.log('connected to Mongodb');
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhook/clerk'],
+  publicRoutes: ['/','/onboarding', '/api/webhook/clerk'],
   ignoredRoutes: ['/api/webhook/clerk']
 });
  
